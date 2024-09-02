9cb6ca4575b1662c89aeadfed65d94d42954583b
jyan500 <jyan500@gmail.com>
2024-09-02 16:55:58 -0700

    added props to render each carousel item as the react node in multi card carousel, added back in the gradient and title of each newspost

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -2,10 +2,8 @@ import React from "react"
 import { Carousel } from "./Carousel"
 import "../styles/dashboard.css"
 import { useAppSelector } from "../hooks/redux-hooks"
-import { convertFlatToNestedArray } from "../helpers/functions"
 import { TestCarousel } from "./TestCarousel"
-import MultiCardCarousel from "./MultiCardCarousel"
-import SingleCardCarousel from "./carousel/SingleCardCarousel"
+import MultiCardCarousel from "./carousel/MultiCardCarousel"
 
 type CarouselElement = {
 	id: number
@@ -15,60 +13,38 @@ type CarouselElement = {
 
 type CarouselContentProps = {
 	data: CarouselElement
-	perPage: number
 }
 
-export const CarouselContent = ({data, perPage}: CarouselContentProps) => {
+export const CarouselContent = ({data}: CarouselContentProps) => {
 	return (
-		// <div>
-		// {/*	<img className = "tw-absolute tw-w-full tw-h-full" alt="" src = {data.imageURL}/>
-		// 	<img src = {data.imageURL}/>
-		// </div>
-		// <div className = {`tw-relative ${perPage > 1 ? "tw-px-2" : ""}`} style={{width: perPage > 1 ? `${100/perPage}%` : "100%"}}>
-		// 	<img src = {data.imageURL}/>
-		// 	{/*<p className = "tw-absolute tw-bottom-3 tw-left-3 tw-text-white tw-text-3xl tw-font-bold">{data.title}</p>*/}
-		// </div>
-		<div className = "tw-w-1/2">
-			<img className = "tw-object-cover" src = {data.imageURL}/>
-			<p>test</p>
-		</div>
-		// <img src = {data.imageURL}/>
+		<>
+	        <img src={data.imageURL} alt={data.title} className="tw-relative tw-object-cover tw-w-full tw-h-full tw-rounded-lg" />
+        	<div className="tw-mx-2 tw-rounded-lg tw-absolute tw-inset-0 tw-bg-gradient-to-b tw-from-transparent tw-to-black tw-from-60%">
+		        <p className = "tw-absolute tw-bottom-4 tw-left-4 tw-text-2xl tw-font-bold tw-text-white">{data.title}</p>
+        	</div>
+        </>
 	)
 }
 
 export const Dashboard = () => {
 	const { newsPosts } = useAppSelector((state) => state.newsPost)
-	const createCarouselElements = (data: Array<CarouselElement>, numPerPage: number, total: number) => {
+	const createCarouselElements = (data: Array<CarouselElement>) => {
 		if (data.length){
 			const carouselElements = data.map((element: CarouselElement) => {
-				return <CarouselContent data={element} perPage={numPerPage}/>
+				return <CarouselContent data={element}/>
 			})
-			// return convertFlatToNestedArray(numPerPage, total, carouselElements)
 			return carouselElements
 		}
 		return []	
 	}
-	const newsPerPage = 2
-	const newsPostElements = createCarouselElements(newsPosts?.length ? newsPosts : [], newsPerPage, newsPosts?.length)
+	const newsPostElements = createCarouselElements(newsPosts?.length ? newsPosts : [])
 	return (
 		<div className = "tw-w-full tw-flex tw-flex-col">
 			<div className = "tw-my-4 tw-px-36 tw-py-14 tw-bg-primary">
 				<p className = "tw-my-1 tw-text-4xl tw-font-bold tw-text-white">Stay Connected</p>	
 				<p className = "tw-text-white">Get the latest updates by subscribing to our eNewsletter!</p>	
 			</div>
-			{/*<div className = "tw-my-4 tw-flex tw-flex-row tw-justify-center">
-				{
-					newsPostElements.length ? (<Carousel data = {newsPostElements as Array<React.ReactNode>} numPerPage = {newsPerPage} total = {newsPostElements.length}/>) : null
-				}
-			</div>*/}
-		{/*	<div className = "tw-flex tw-flex-row tw-justify-center">
-				{
-					newsPostElements.length ? (<TestCarousel data={newsPostElements as Array<Array<React.ReactNode>>} numPerPage = {newsPerPage} total = {newsPostElements.length}/>) : null
-				}
-			</div>*/}
-			{/*<MultiCardCarousel items={newsPosts.map((post)=>{ return {id: post.id, imageURL: post.imageURL, title: post.title}})}/>*/}
-			<MultiCardCarousel itemsPerPage={2}/>
-			{/*<SingleCardCarousel/>*/}
+			<MultiCardCarousel items={newsPostElements} itemsPerPage={1}/>
 		</div>
 	)	
 }
---
+++ client/src/components/carousel/MultiCardCarousel.tsx
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from 'react';
+import { IconContext } from "react-icons"
+import { GrPrevious as Previous } from "react-icons/gr";
+import { GrNext as Next } from "react-icons/gr";
+import { v4 as uuidv4 } from "uuid"
+
+type Props = {
+    items: Array<React.ReactNode>
+    itemsPerPage: number
+}
+
+const MultiCardCarousel = ({ items, itemsPerPage }: Props) => {
+
+    const [currentIndex, setCurrentIndex] = useState(0)
+
+    if (itemsPerPage < 1 || itemsPerPage > items.length) {
+        return <div className = "tw-font-bold tw-text-red-500">Carousel could not render. <code>itemsPerPage</code> {"must be >= 1 and <= items.length"}</div>
+    }
+
+    const style = {
+        ...(itemsPerPage > 1 ? {width: `${1/itemsPerPage*100}%`} : {})
+    }
+
+    const handlePrev = () => {
+        /* 
+            the total number of pages = Math.ceil(items.length/itemsPerPage), so 
+            the last index should be total number of pages - 1
+        */
+        const newIndex = currentIndex === 0 ? Math.ceil(items.length/itemsPerPage) - 1 : currentIndex - 1 
+        setCurrentIndex(newIndex)
+    }
+
+    const handleNext = () => {
+        const newIndex = currentIndex === Math.ceil(items.length/itemsPerPage) - 1 ? 0 : currentIndex + 1
+        setCurrentIndex(newIndex)
+    }
+
+    return (
+        <div>
+            <div className = "tw-px-36 tw-flex tw-justify-center">
+                <div className={`${itemsPerPage === 1 ? "tw-max-h-[600px] tw-max-w-[800px]" : ""} tw-relative tw-h-full tw-w-full tw-overflow-hidden tw-rounded-lg`}>
+                    {/* 
+                        transition translate X based on the -(currentIndex) * 100. 
+                        it's negative so the animation will start from right to left
+                    */}
+                    <div className="tw-flex tw-transition-transform tw-duration-700 tw-ease-in-out" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+                        {items.map((item) => (
+                            /* 
+                                Renders all items, the combination of flex-shrink-0 on each flex item, as well as the width being set to the percent each item takes per page
+                                gives the illusion that only a certain amount of items display on each page based on the itemsPerPage.
+                            */
+                            <div key={uuidv4()} style = {style} className = "tw-relative tw-w-full tw-h-full tw-flex-shrink-0 tw-px-2">
+                                {item}
+                            </div>
+                        ))}
+                    </div>
+                    {
+                        itemsPerPage < items.length ? (
+                            <>
+                            <button
+                                className="tw-absolute tw-top-1/2 tw-left-4 tw-transform -tw-translate-y-1/2 hover:tw-bg-white hover:tw-opacity-40 tw-bg-white tw-opacity-60 tw-text-gray-800 tw-px-2 tw-py-2 tw-rounded-full tw-cursor-pointer"
+                                onClick={handlePrev}
+                            >
+                                <IconContext.Provider value = {{className: "tw-w-6 tw-h-6"}}>
+                                    <Previous/> 
+                                </IconContext.Provider> 
+                            </button>
+                            <button
+                                className="tw-absolute tw-top-1/2 tw-right-4 tw-transform -tw-translate-y-1/2 hover:tw-bg-white hover:tw-opacity-40 tw-bg-white tw-opacity-60 tw-text-gray-800 tw-px-2 tw-py-2 tw-rounded-full tw-cursor-pointer"
+                                onClick={handleNext}
+                            >
+                                <IconContext.Provider value = {{className: "tw-w-6 tw-h-6"}}>
+                                    <Next/>
+                                </IconContext.Provider> 
+                            </button>
+                            </>
+                        ) : null
+                    }
+                </div>
+            </div>
+            <div className = "tw-flex tw-flex-col tw-justify-center">
+                {
+                    itemsPerPage < items.length ? (
+                        <div className = "tw-p-4 tw-flex tw-items-center tw-justify-center tw-gap-2">
+                            {
+                                Array.from(Array(Math.ceil(items.length/itemsPerPage)), (_, i) => {
+                                    return (
+                                        <div className = {`tw-transition tw-w-3 tw-h-3 tw-bg-gray-800 tw-rounded-full ${currentIndex === i ? "tw-p-2" : "tw-bg-opacity-50"}`}></div>
+                                    )
+                                })
+                            }   
+                        </div>
+                    ) : null
+                } 
+            </div>
+        </div>
+    )
+}
+
+export default MultiCardCarousel
